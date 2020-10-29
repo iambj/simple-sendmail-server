aacf8550490ec9596a3cf713cf1f23a5a06982ef
iambj <iambj@icloud.com>
2020-10-28 22:32:31 -0400

    Cleaned up the module.

--- wordFilter.js
+++ wordFilter.js
@@ -4,17 +4,13 @@ const config = require("config");
 let flagged = [];
 let profanity = [];
 
+// Load the vulgarities into memory
 fs.readFile("./wordLists/flaggedWords.txt", (err, data) => {
     if (err) {
         console.log(err);
         return;
     }
-    // console.log("words", data.toString());
     flagged = data.toString().trim().split("\n");
-    // console.log(flagged);
-    let msg = "This is a spam message fuck cunt free gift nigerian";
-
-    // console.log(checkList(msg));
 });
 
 fs.readFile("./wordLists/swearWords.txt", (err, data) => {
@@ -22,12 +18,8 @@ fs.readFile("./wordLists/swearWords.txt", (err, data) => {
         console.log(err);
         return;
     }
-    // console.log("words", data.toString());
+    // console.log(profanity); // TODO trim() actually isn't trimming
     profanity = data.toString().trim().split("\n");
-    let msg = "This is a spam message fuck cunt";
-
-    // console.log();
-    // console.log(profanity);
 });
 
 function checkList(data) {
@@ -35,24 +27,13 @@ function checkList(data) {
     let swears;
     if (config.get("useLanguageFilter")) {
         swears = profanity.filter((word) => {
-            // console.log(word);
             let regEx = new RegExp("\\b" + word + "\\b");
-            // let regEx = new RegExp(word);
             return data.match(regEx);
         });
     }
-    // let word = "nigerian";
-    // let regEx = new RegExp(`\b${word}\b`, "gmi");
-    // console.log(
-    //     "<b>This is anemail from the Pettinontheritz.com contact form:</b><br/><ul><li><b>Name:</b> brandon</li><li><b>Email:</b> iambj@icloud.com</li><li><b>Phone:</b> 111-111-1111</li><li><b>Message:</b> Message in JSON nigerian</li></ul>'".match(
-    //         /\bnigerian\b/
-    //     )
-    // );
     if (config.get("useSpamWordFilter")) {
         spam = flagged.filter((word) => {
-            // console.log(word);
             let regEx = new RegExp("\\b" + word + "\\b", "g");
-            // console.log(regEx);
             return data.match(regEx);
         });
     }
